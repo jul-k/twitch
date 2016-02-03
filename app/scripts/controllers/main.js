3dd2cfd45a1b591d5394305a7f5e41cb72f4842e
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-03 16:57:04 +0200

    Ready to start

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twitchTvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twitchTvApp
+ */
+angular.module('twitchTvApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
