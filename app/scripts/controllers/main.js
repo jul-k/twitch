4ce1de9447176824e85b9f898ce21e6271602e16
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-05 16:59:07 +0200

    Fetch Twitch channels' data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,4 +12,18 @@ var app = angular.module('twitchTvApp');
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
 
+    $scope.streamingUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "mr_coding"];
+
+    $scope.streamsList = [];
+
+    for (var i=0; i<$scope.streamingUsers.length; i++) {
+        $.getJSON('https://api.twitch.tv/kraken/streams/'+
+        $scope.streamingUsers[i] +
+        '?callback=?', function(data) {
+            console.log(data);
+            $scope.streamsList.push(data.stream);
+        });
+    }
+
+
 }]);
