0f56f106f064c54aa3a85f2abca2723c1ccfdd83
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-06 21:44:39 +0200

    Almost ready

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,18 +12,30 @@ var app = angular.module('twitchTvApp');
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
 
-    $scope.streamingUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "mr_coding"];
+    $scope.streamingUsers = ["freecodecamp", "riotgames", "forsenlol", "nextgentactics", "kinggothalion", "kingluii718", "chainerfails", "kungentv", "cloudmighty", "jendenise", "uknighted", "mattmelvin", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "mr_coding"];
 
     $scope.streamsList = [];
 
     for (var i=0; i<$scope.streamingUsers.length; i++) {
+        var userName = $scope.streamingUsers[i];
         $.getJSON('https://api.twitch.tv/kraken/streams/'+
-        $scope.streamingUsers[i] +
-        '?callback=?', function(data) {
-            console.log(data);
-            $scope.streamsList.push(data.stream);
-        });
+        userName +
+        '?callback=?', updateFor(userName));
     }
+
+    function updateFor(userName) {
+        return function(data) {
+            data.name = userName;
+            data.isOnline = true;
+            if (data.stream==null) {
+                data.isOnline = false;
+            }
+            console.log(data);
+            $scope.streamsList.push(data);
+            $scope.$apply();
+        }
+    }
+
 
 
 }]);
